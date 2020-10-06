5fa6f8591b1f853c5b380a75e207d74dfb8be294
LolWastedJS <ml2dofficial@gmail.com>
2020-10-05 17:40:48 -0700

    ✨️ create bitfield classes for stuff

---
+++ src/discord/bitfields/Intents.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2020. MeLike2D All Rights Reserved.
+ * Neo is licensed under the MIT License.
+ * See the LICENSE file in the project root for more details.
+ */
+
+import { BitField, BitFieldObject } from "../Bitfield";
+
+export enum Intent {
+  Guilds = 1 << 0,
+  GuildMembers = 1 << 1,
+  GuildBans = 1 << 2,
+  GuildEmojis = 1 << 3,
+  GuildIntegrations = 1 << 4,
+  GuildWebhooks = 1 << 5,
+  GuildInvites = 1 << 6,
+  GuildVoiceStates = 1 << 7,
+  GuildPresences = 1 << 8,
+  GuildMessages = 1 << 9,
+  GuildMessageReactions = 1 << 10,
+  GuildMessageTyping = 1 << 11,
+  DirectMessages = 1 << 12,
+  DirectMessageReactions = 1 << 13,
+  DirectMessageTyping = 1 << 14,
+}
+
+export class Intents extends BitField<IntentResolvable> {
+  /**
+   * All intents that were provided by discord.
+   */
+  public static FLAGS = Intent;
+  /**
+   * All privileged intents ORed together.
+   */
+  public static PRIVILEGED = Intent.GuildMembers | Intent.GuildPresences;
+
+  /**
+   * All of the non-privileged intents.
+   */
+  public static NON_PRIVILEGED = Intents.ALL & ~Intents.PRIVILEGED;
+
+  /**
+   * Recommended defaults by NeoCord.
+   */
+  public static DEFAULT =
+    Intent.Guilds |
+    Intent.GuildMessages |
+    Intent.GuildBans |
+    Intent.GuildEmojis |
+    Intent.GuildInvites |
+    Intent.GuildVoiceStates |
+    Intent.DirectMessages;
+}
+
+export type IntentResolvable =
+  | Intent
+  | keyof typeof Intent
+  | number
+  | BitFieldObject
+  | (keyof typeof Intent | number | BitFieldObject)[];
---
+++ src/discord/bitfields/UserFlags.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020. MeLike2D All Rights Reserved.
+ * Neo is licensed under the MIT License.
+ * See the LICENSE file in the project root for more details.
+ */
+
+import { BitField, BitFieldObject } from "../Bitfield";
+
+export enum UserFlag {
+  DiscordEmployee = 1 << 0,
+  DiscordPartner = 1 << 1,
+  HypeSquadEvents = 1 << 2,
+  BugHunterLevelOne = 1 << 3,
+  HouseBravery = 1 << 6,
+  HouseBrilliance = 1 << 7,
+  HouseBalance = 1 << 8,
+  EarlySupporter = 1 << 9,
+  TeamUser = 1 << 10,
+  System = 1 << 12,
+  BugHunterLevelTwo = 1 << 14,
+  VerifiedBot = 1 << 16,
+  VerifiedBotDeveloper = 1 << 17,
+}
+
+export class UserFlags extends BitField<UserFlagResolvable> {}
+
+export type UserFlagResolvable =
+  | UserFlag
+  | keyof typeof UserFlag
+  | number
+  | BitFieldObject
+  | (keyof typeof UserFlag | number | BitFieldObject)[];
